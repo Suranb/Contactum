787fc4cc2bfe3887877b8eb35acfd2ad9b2ab84a
Suran Basharati <suranbasharati@gmail.com>
2025-12-22 15:47:40 +0100

    http file can now call on the API and create a company

--- src/Contactum.Application/obj/Debug/net10.0/Contactum.Application.AssemblyInfo.cs
+++ src/Contactum.Application/obj/Debug/net10.0/Contactum.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contactum.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19fd55b64e660fae3903708595aa7d678c5bde9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b10c63130c41f9b5ebf070ba319715e2544cd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contactum.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contactum.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
